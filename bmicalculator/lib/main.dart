4dc0a9aa3770dce184a7ea2965d20d9b8da6b963
thenepalivideo <thenepalivideo@gmail.com>
2022-07-28 16:26:00 +0545

    First cal

---
+++ bmicalculator/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BmiCalculator(),
+    );
+  }
+}
+
+class BmiCalculator extends StatefulWidget {
+  const BmiCalculator({Key? key}) : super(key: key);
+
+  @override
+  State<BmiCalculator> createState() => _BmiCalculatorState();
+}
+
+class _BmiCalculatorState extends State<BmiCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
